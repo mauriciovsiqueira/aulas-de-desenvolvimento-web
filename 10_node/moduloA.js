99005357e398dafefee4cdfdb1e9876e135ee041
Maurício Siqueira <mauriciovsiqueira@outlook.com.br>
2026-01-21 14:49:28 -0300

    docs: atualização da aula.

--- 10_node/moduloA.js
+++ 10_node/moduloA.js
@@ -1,3 +1,7 @@
 this.ola = "Fala Pessoal";
 exports.bemVindo = "Bem vindo ao node!";
-MediaSourceHandle.exports.ateLogo = "Até próximo exemplo";
+module.exports.ateLogo = "Até próximo exemplo";
+
+// this.ola: No escopo global de um módulo Node, o this referencia o objeto module.exports.
+// exports.bemVindo: O exports é um "atalho" (shorthand) que aponta para module.exports. É a forma mais comum de exportar múltiplas funções ou variáveis.
+// module.exports.ateLogo: É a forma oficial e principal. O Node.js realmente retorna o que estiver contido em module.exports quando você usa require() em outro arquivo.
--- 10_node/moduloCliente.js
+++ 10_node/moduloCliente.js
@@ -1,1 +1,12 @@
+// require - ele seleciona o arquivo com os modulos - ./modulo é o arquivo e onde ele está.
 const moduloA = require("./moduloA");
+const moduloB = require("./moduloB");
+
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.ateLogo);
+console.log(moduloA);
+
+console.log(moduloB.bomDia);
+console.log(moduloB.boaNoite());
+console.log(moduloB);
