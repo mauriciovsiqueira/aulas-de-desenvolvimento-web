eb04d70e9afff4aee7105b7e696d9f6ccce272ca
Maurício Siqueira <mauriciovsiqueira@outlook.com.br>
2026-01-21 15:11:42 -0300

    docks: criação de novos arquivos

---
+++ 10_node/01_node.js
@@ -0,0 +1,12 @@
+// node -v - para verificar a versão do node.
+
+// npm - é um gerenciador de pacotes do node.
+// npm -v - para verificar a versão.
+// npm i ou npm install - instalar os pacotes.
+// npm i lodash - instala essa biblioteca - ele instalou e criou a pasta node_modules
+
+// git/github
+// Criação do .gitignore
+// ele serve para bloquear arquivos que não vão para o github.
+// Criar arquivo .gitignore e nela colocar isso:
+// node_modules/
--- 10_node/moduloCliente.js
+++ 10_node/moduloCliente.js
@@ -1,4 +1,15 @@
-// require - ele seleciona o arquivo com os modulos - ./modulo é o arquivo e onde ele está.
+// require:
+// Você pode usar o require para carregar três tipos de recursos:
+// Módulos Internos (Core Modules): Já vêm com o Node.
+// const fs = require('fs'); // Sistema de arquivos
+// const path = require('path');
+
+// Módulos Locais: Arquivos criados por você no projeto (é obrigatório usar o caminho relativo ./):
+// const meuModulo = require('./meuModulo');
+
+// Módulos de Terceiros: Instalados via NPM (ficam na pasta node_modules):
+// const express = require('express');
+
 const moduloA = require("./moduloA");
 const moduloB = require("./moduloB");
 
